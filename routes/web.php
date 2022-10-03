3c6cc20fafc3af6d49f5d3e13722430797173e4c
Wellyngton Douglas <wellyngtond159@gmail.com>
2022-10-02 22:17:36 -0300

    adding comments on routes

--- routes/web.php
+++ routes/web.php
@@ -37,43 +37,35 @@ Route::middleware('autenticacao:padrao,visitante,p3,p4') -> prefix('app') -> gro
   Route::get('/home', [HomeController::class, 'index']) -> name('app.home');
   Route::get('/sair', [LoginController::class, 'sair']) -> name('app.sair');
   Route::get('/cliente', [ClienteController::class, 'index']) -> name('app.cliente');
-
+  
+  //fornecedor
   Route::get('/fornecedor', [FornecedorController::class, 'index']) -> name('app.fornecedor');
-
   Route::get('/fornecedor/listar', [FornecedorController::class, 'listar']) -> name('app.fornecedor.listar');
   Route::post('/fornecedor/listar', [FornecedorController::class, 'listar']) -> name('app.fornecedor.listar');
-
   Route::get('/fornecedor/editar/{id}/{msg?}', [FornecedorController::class, 'editar']) -> name('app.fornecedor.editar');
   Route::get('/fornecedor/excluir/{id}', [FornecedorController::class, 'excluir']) -> name('app.fornecedor.excluir');
-  
   Route::get('/fornecedor/adicionar', [FornecedorController::class, 'adicionar']) -> name('app.fornecedor.adicionar');
   Route::post('/fornecedor/adicionar', [FornecedorController::class, 'adicionar']) -> name('app.fornecedor.adicionar');
   
-
+  //produto
   Route::get('/produto', [ProdutoController::class, 'index']) -> name('app.produto');
-
   Route::get('/produto/adicionar', [ProdutoController::class, 'adicionar']) -> name('app.produto.adicionar');
   Route::post('/produto/adicionar', [ProdutoController::class, 'adicionar']) -> name('app.produto.adicionar');
-
   Route::get('/produto/listar', [ProdutoController::class, 'listar']) -> name('app.produto.listar');
-  
   Route::get('/produto/editar/{id}/{msg?}', [ProdutoController::class, 'editar']) -> name('app.produto.editar');
   Route::get('/produto/excluir/{id}', [ProdutoController::class, 'excluir']) -> name('app.produto.excluir');
 
+  //detalhes do produto
   Route::get('/produto/detalhe/adicionar/{id_produto?}', [ProdutoDetalheController::class, 'adicionar']) -> name('app.produto_detalhe.adicionar');
   Route::post('/produto/detalhe/adicionar', [ProdutoDetalheController::class, 'adicionar']) -> name('app.produto_detalhe.adicionar');
-  
   Route::get('/produto/detalhe/listar/{id}', [ProdutoDetalheController::class, 'listar']) -> name('app.produto_detalhe');
-  
   Route::get('/produto/detalhe/editar/{id}/{id_produto}/{msg?}', [ProdutoDetalheController::class, 'editar']) -> name('app.produto_detalhe.editar');
   Route::get('/produto/detalhe/excluir/{id}', [ProdutoDetalheController::class, 'excluir']) -> name('app.produto_detalhe.excluir');
 
-
+  //unidades
   Route::get('/produto/unidade/listar', [UnidadeController::class, 'listar']) -> name('app.unidade.listar');
-  
   Route::get('/produto/unidade/adicionar', [UnidadeController::class, 'adicionar']) -> name('app.unidade.adicionar');
   Route::post('/produto/unidade/adicionar', [UnidadeController::class, 'adicionar']) -> name('app.unidade.adicionar');
-
   Route::get('/produto/unidade/editar/{id}/{msg?}', [UnidadeController::class, 'editar']) -> name('app.unidade.editar');
   Route::get('/produto/unidade/excluir{id}', [UnidadeController::class, 'excluir']) -> name('app.unidade.excluir');
 });
